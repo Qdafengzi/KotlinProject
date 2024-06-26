337784aa4ce3e1e891167083cdac76853e1d5792
finnq <qdafengzi@gmail.com>
2024-06-26 18:35:52 +0800

    update

---
+++ composeApp/src/androidMain/kotlin/bridge/Bar.android.kt
@@ -0,0 +1,19 @@
+package bridge
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+
+
+@Composable
+actual fun PlatformColors(statusBarColor: Color, navBarColor: Color){
+//    val sysUiController = rememberSystemUiController()
+//        SideEffect {
+//            sysUiController.setSystemBarsColor(color = topColor)
+//            sysUiController.setNavigationBarColor(color = bottomColor)
+//        }
+}
+
+
+
+
---
+++ composeApp/src/commonMain/kotlin/bridge/Bar.kt
@@ -0,0 +1,9 @@
+package bridge
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+expect fun PlatformColors(statusBarColor: Color, navBarColor: Color)
+
